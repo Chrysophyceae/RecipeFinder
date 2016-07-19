ace8a46b140afbc199fccb42a8dd4c493b950287
Chrysophyceae <barbara.findenig@outlook.com>
2016-07-19 22:32:40 +0200

    test

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rezept kuchen = new Rezept();
+            
+
+
+
+        }
+    }
+}
+    class Rezept
+    {
+    
+    string[] zutaten = new string[]
+    {
+
+    };
+
+}
---
+++ ConsoleApplication2/RezeptModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class RezeptModel
+    {
+        int MehlinGramm = 0;
+        int ZuckerinGramm = 0;
+        int ButterinGramm = 0;
+        int EierStueck = 0;
+  
+        public void SetZutatMehl(int MehlVorhanden)
+        {
+           MehlinGramm = MehlVorhanden;
+        }
+
+        public void SetZutatZucker(int ZuckerVorhanden)
+        {
+            ZuckerinGramm = ZuckerVorhanden;
+        }
+
+        public void SetZutatButter(int ButterVorhanden)
+        {
+            ButterinGramm = ButterVorhanden;
+        }
+
+        public void SetZutatEier(int EierVorhanden)
+        {
+            EierStueck = EierVorhanden;
+        }
+
+        public int GetZutatMehl()
+        {
+            return MehlinGramm;
+        }
+
+        public int GetZutatZucker()
+        {
+            return ZuckerinGramm;
+        }
+
+        public int GetZutatButter()
+        {
+            return ButterinGramm;
+        }
+
+        public int GetZutatEier()
+        {
+            return EierStueck;
+        }
+    }
+}
